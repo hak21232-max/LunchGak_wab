969c8cd5c0af13f3fe0d820f41c91c7de5fd4685
hak21232 <hak21232@gmail.com>
2026-07-07 15:24:32 +0900

    Functions 백엔드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import GuideArticle from './pages/GuideArticle'
 import BlogIndex from './pages/BlogIndex'
 import BlogPost from './pages/BlogPost'
 import LocationSelect from './pages/LocationSelect'
+import SharedResult from './pages/SharedResult'
 import ExcludedRestaurants from './pages/ExcludedRestaurants'
 
 function AppShell() {
